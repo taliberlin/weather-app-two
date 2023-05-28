4b0b3df948337a95c3e53079b48e425444264b73
taliberlin <118308290+taliberlin@users.noreply.github.com>
2023-05-28 13:58:57 -0700

    cleaned up code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <footer>
+        🤘🏼 This site was coded by Tali Berlin and is{" "}
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/taliberlin/weather-app-two"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noreferrer"
         >
-          Learn React
+          open-sourced
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
-
-export default App;
