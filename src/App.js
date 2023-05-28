be059336ad22fac61c49d9ebe32a2201c82075d3
taliberlin <118308290+taliberlin@users.noreply.github.com>
2023-05-28 14:28:22 -0700

    added HTML

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <footer>
-        🤘🏼 This site was coded by Tali Berlin and is{" "}
-        <a
-          href="https://github.com/taliberlin/weather-app-two"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced
-        </a>
-      </footer>
+      <div className="container">
+        <Weather />
+        <footer>
+          🤘🏼 This site was coded by Tali Berlin and is{" "}
+          <a
+            href="https://github.com/taliberlin/weather-app-two"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter city..."
+              autoFocus
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h1>Vancouver</h1>
+      <ul>
+        <li>Sunday 14:03</li>
+        <li>Partly Sunny</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="Partly Cloudy"
+          />
+          6°C
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 15%</li>
+            <li>Humidity: 72%</li>
+            <li>Wind: 13km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
