8c6e7a1f549feeaa533e7aeb7ab634b0f0b45f94
taliberlin <118308290+taliberlin@users.noreply.github.com>
2023-06-04 22:18:53 -0700

    edited forecast spacing

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -18,7 +18,7 @@ export default function WeatherForecast(props) {
   if (loaded) {
     return (
       <div className="WeatherForecast">
-        <div className="row mt-2">
+        <div className="row mt-1">
           {forecast.map(function (dailyForecast, index) {
             if (index < 5) {
               return (
